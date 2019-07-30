51439e589ec5dd33d20f4d1542c1ed5528c3b208
habout <hossain.aboutalebi@gmail.com>
2019-07-30 19:43:01 -0400

    Merge branch 'SGD' of https://github.com/h-aboutalebi/pytorch-ddpg-naf into Reacher
    # Conflicts:
    #	policy_engine/poly_rl.py

--- policy_engine/graph.py
+++ policy_engine/graph.py
@@ -13,43 +13,48 @@ class Create_Graph:
         self.Number_of_iteration =  Number_of_iteration
         self.max_x_number = max_x_number
         self.min_x_number = min_x_number
+        string_directory="/Users/hosseinaboutalebi/Desktop/Git_hub/pytorch-ddpg-naf/files_non_sparce"
         self.x_values = [i for i in range(0, max_x_number, 10)]
         PolyRL_result = [[] for _ in range(Number_of_iteration)]
         ParamNoise_result = [[] for _ in range(Number_of_iteration)]
         DDPG_result = [[] for _ in range(Number_of_iteration)]
-        self.get_results_from_file(PolyRL_result, string="files/PolyRL")
-        self.get_results_from_file(ParamNoise_result, string="files3/param_noise")
-        self.get_results_from_file(DDPG_result, string="files/DDPG")
+        Div_Dis_result = [[[0 for _ in range(0, 1000, 10)]] for _ in range(Number_of_iteration)]
+        self.get_results_from_file(PolyRL_result, string=string_directory+"/PolyRL")
+        self.get_results_from_file(ParamNoise_result, string=string_directory+"/param_noise")
+        self.get_results_from_file(DDPG_result, string=string_directory+"/DDPG")
         # self.get_results_from_file(DDPG_result, string="files2/DDPG")
-        self.plot_figure(PolyRL_result, DDPG_result,ParamNoise_result)
+        self.plot_figure(PolyRL_result, DDPG_result,ParamNoise_result,Div_Dis_result)
 
     def get_results_from_file(self, list, string):
         for i in range(self.Number_of_iteration):
             infile = open(string + str(i+1) + ".pkl", 'rb')
             example_dict = pickle.load(infile)
-            self.x_new_values, power_smooth = self.make_smooth_line(example_dict['modified_reward'])
+            self.x_new_values, power_smooth = self.make_smooth_line(example_dict['modified_reward'][:int(self.max_x_number/10)])
             list[i].append(power_smooth)
 
     # implementation from https://stackoverflow.com/questions/5283649/plot-smooth-line-with-pyplot
     def make_smooth_line(self, list):
         xnew = np.linspace(self.min_x_number, self.max_x_number,
                            len(list))  # 300 represents number of points to make between T.min and T.max
-        spl = make_interp_spline(self.x_values, list, k=3)  # BSpline object
-        power_smooth = spl(xnew)
-        return xnew, power_smooth
+        # spl = make_interp_spline(self.x_values, list, k=2)  # BSpline object
+        # power_smooth = spl(xnew)
+        return xnew, np.array(list)/400
 
-    def plot_figure(self, PolyRL_result, DDPG_result,ParamNoise_result):
+    def plot_figure(self, PolyRL_result, DDPG_result,ParamNoise_result,Div_Dis_result):
         y_PolyRL_result = np.mean(PolyRL_result, axis=0)
         error_PolyRL_result = stats.sem(PolyRL_result, axis=0)
         y_ParamNoise_result = np.mean(ParamNoise_result, axis=0)
         error_ParamNoise_result = stats.sem(ParamNoise_result, axis=0)
         y_DDPG_result = np.mean(DDPG_result, axis=0)
         error_DDPG_result = stats.sem(DDPG_result, axis=0)
+        y_Div_Dis_result = np.mean(Div_Dis_result, axis=0)
+        error_Div_Dis_result_result = stats.sem(Div_Dis_result, axis=0)
         plt.xlabel('Episodes')
         plt.ylabel('Reward')
         plt.plot(self.x_new_values, y_PolyRL_result[0], "b",label='DDPG with PolyRL')
         plt.plot(self.x_new_values, y_ParamNoise_result[0], "r", label='DDPG with Parameter Noise')
         plt.plot( self.x_new_values, y_DDPG_result[0], "g", label='DDPG')
+        plt.plot(self.x_new_values, y_Div_Dis_result[0], "orange", label='Div-DDPG')
         plt.fill_between(self.x_new_values, y_PolyRL_result[0] - error_PolyRL_result[0], y_PolyRL_result[0] + error_PolyRL_result[0], edgecolor='#0000FF',
                          facecolor='#0000FF', alpha=0.5,
                          linewidth=0)
@@ -59,7 +64,10 @@ class Create_Graph:
                          linewidth=0)
         plt.fill_between(self.x_new_values, y_DDPG_result[0] - error_DDPG_result[0], y_DDPG_result[0] + error_DDPG_result[0], edgecolor='#3F7F4C', facecolor='#3F7F4C', alpha=0.5,
                          linewidth=0)
+        plt.fill_between(self.x_new_values, y_Div_Dis_result[0] - error_Div_Dis_result_result[0], y_Div_Dis_result[0] + error_Div_Dis_result_result[0], edgecolor='#3F7F4C',
+                         facecolor='#3F7F4C', alpha=0.5,
+                         linewidth=0)
 
         plt.show()
 
-Create_Graph(Number_of_iteration=5)
+Create_Graph(Number_of_iteration=2)
