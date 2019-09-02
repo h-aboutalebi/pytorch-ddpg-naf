806a0020fdff0eeebae1b7c0758e1787b20e8e5b
Hossein Aboutalebi <hossain.aboutalebi@gmail.com>
2019-09-02 10:36:26 -0400

    Update mod_reward.py

--- policy_engine/mod_reward.py
+++ policy_engine/mod_reward.py
@@ -4,7 +4,7 @@ import torch
 def make_reward_sparse(env,reward,flag_sparse,threshold_sparcity,initial_pose):
     flag_absorbing_state=False
     if flag_sparse is True:
-        if (abs(env.env.data.qpos[0]-initial_pose)>=threshold_sparcity):
+        if (abs(env.env.sim.qpos[0]-initial_pose)>=threshold_sparcity):
             reward = reward
             # flag_absorbing_state=True
         else:
